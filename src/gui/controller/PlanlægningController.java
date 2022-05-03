3ab38c22147d4b735c6a80f3b429069cefaee4f5
Tobias Rasmussen <tobi9782@easv365.dk>
2022-05-03 13:27:02 +0200

    add

---
+++ src/gui/controller/PlanlægningController.java
@@ -0,0 +1,24 @@
+package gui.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextArea;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class PlanlægningController implements Initializable {
+
+
+    @FXML
+    private TextArea txtAreaPlanlægning;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    public void handelVidere(ActionEvent actionEvent) {
+    }
+}
