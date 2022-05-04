1cc9c23e80f9e7fa618838fee318809bd522c92e
Tobias Rasmussen <tobi9782@easv365.dk>
2022-05-04 09:36:29 +0200

    add

--- src/dal/DBScript/FSlll_DataCredential.sql
+++ src/dal/DBScript/FSlll_DataCredential.sql
@@ -4,4 +4,7 @@ GO
 INSERT INTO [Person] (FirstName, LastName, Role) VALUES ('Tobias', 'Rasmussen', 'STUDENT')
 
 
-INSERT INTO [Credentials] (ID,UserName,Password) VALUES (1,'tobias', 'password')
+INSERT INTO [Credentials] (ID,UserName,Password) VALUES (1,'tobias', '$2a$10$YvmNIkjXaNy9CJFDCEi09uAGVnNgMs353F9tCGTkq9NHwtPCGjsAy')
+INSERT INTO [Credentials] (ID,UserName,Password) VALUES (1,'jan', '$2a$10$YvmNIkjXaNy9CJFDCEi09uAGVnNgMs353F9tCGTkq9NHwtPCGjsAy')
+INSERT INTO [Credentials] (ID,UserName,Password) VALUES (1,'mikkel', '$2a$10$YvmNIkjXaNy9CJFDCEi09uAGVnNgMs353F9tCGTkq9NHwtPCGjsAy')
+INSERT INTO [Credentials] (ID,UserName,Password) VALUES (1,'simon', '$2a$10$YvmNIkjXaNy9CJFDCEi09uAGVnNgMs353F9tCGTkq9NHwtPCGjsAy')
