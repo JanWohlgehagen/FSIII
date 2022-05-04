70280b4ce0d5d7142c07b09a95efab90af2aa041
Tobias Rasmussen <tobi9782@easv365.dk>
2022-05-04 09:37:41 +0200

    simon vil have et password p

--- src/dal/DBScript/FSlll_DataCredential.sql
+++ src/dal/DBScript/FSlll_DataCredential.sql
@@ -7,4 +7,4 @@ INSERT INTO [Person] (FirstName, LastName, Role) VALUES ('Tobias', 'Rasmussen', 
 INSERT INTO [Credentials] (ID,UserName,Password) VALUES (1,'tobias', '$2a$10$YvmNIkjXaNy9CJFDCEi09uAGVnNgMs353F9tCGTkq9NHwtPCGjsAy')
 INSERT INTO [Credentials] (ID,UserName,Password) VALUES (1,'jan', '$2a$10$YvmNIkjXaNy9CJFDCEi09uAGVnNgMs353F9tCGTkq9NHwtPCGjsAy')
 INSERT INTO [Credentials] (ID,UserName,Password) VALUES (1,'mikkel', '$2a$10$YvmNIkjXaNy9CJFDCEi09uAGVnNgMs353F9tCGTkq9NHwtPCGjsAy')
-INSERT INTO [Credentials] (ID,UserName,Password) VALUES (1,'simon', '$2a$10$YvmNIkjXaNy9CJFDCEi09uAGVnNgMs353F9tCGTkq9NHwtPCGjsAy')
+INSERT INTO [Credentials] (ID,UserName,Password) VALUES (1,'simon', '$2a$10$fDGJS/UbXJ3QmJ6A5YYx9OGgHX0WZF6MIqSghr1/hInhyhHsRCIRq')
