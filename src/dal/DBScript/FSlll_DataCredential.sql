c747919f2191294dc8b7f7bd96b069afdde0f6da
Tobias Rasmussen <tobi9782@easv365.dk>
2022-05-06 12:28:32 +0200

    add

--- src/dal/DBScript/FSlll_DataCredential.sql
+++ src/dal/DBScript/FSlll_DataCredential.sql
@@ -12,12 +12,12 @@ INSERT INTO [Credentials] (ID,UserName,Password) VALUES (2,'jan', '$2a$10$YvmNIk
 INSERT INTO [Credentials] (ID,UserName,Password) VALUES (3,'mikkel', '$2a$10$YvmNIkjXaNy9CJFDCEi09uAGVnNgMs353F9tCGTkq9NHwtPCGjsAy')
 INSERT INTO [Credentials] (ID,UserName,Password) VALUES (4,'simon', '$2a$10$fDGJS/UbXJ3QmJ6A5YYx9OGgHX0WZF6MIqSghr1/hInhyhHsRCIRq')
 
-INSERT INTO Borger (FirstName, LastName, Age, Template) VALUES ('Rosa', 'Jensen', 63, false)
-INSERT INTO Borger (FirstName, LastName, Age, Template) VALUES ('Jan', 'Olsen', 56, false)
-INSERT INTO Borger (FirstName, LastName, Age, Template) VALUES ('Simon', 'Jensen', 40, false)
-INSERT INTO Borger (FirstName, LastName, Age, Template) VALUES ('Jo', 'Hansen', 90, false)
-INSERT INTO Borger (FirstName, LastName, Age, Template) VALUES ('Bo', 'Rasmussen', 80, false)
-INSERT INTO Borger (FirstName, LastName, Age, Template) VALUES ('Bent', 'Z', 83, false)
+INSERT INTO Borger (FirstName, LastName, Age, Template) VALUES ('Rosa', 'Jensen', 63, 0)
+INSERT INTO Borger (FirstName, LastName, Age, Template) VALUES ('Jan', 'Olsen', 56, 0)
+INSERT INTO Borger (FirstName, LastName, Age, Template) VALUES ('Simon', 'Jensen', 40, 0)
+INSERT INTO Borger (FirstName, LastName, Age, Template) VALUES ('Jo', 'Hansen', 90, 0)
+INSERT INTO Borger (FirstName, LastName, Age, Template) VALUES ('Bo', 'Rasmussen', 80, 0)
+INSERT INTO Borger (FirstName, LastName, Age, Template) VALUES ('Bent', 'Z', 83, 0)
 
 INSERT INTO [Case] (Borger_ID, Title, Description, Bevilling, Bevillings_Tekst, [Plan], Opfoelgnings_Tag)
 VALUES (1, 'braket Arm', 'Det ved ikke', 0,'test', 'Der er ikke en plan', 'Fortsættes')
